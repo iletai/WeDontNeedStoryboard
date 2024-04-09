2bc29b0ce4342d6bcfe44a5635435128065dbb05
iletai <iletai@hotmail.com>
2024-04-09 16:54:35 +0700

    + Add MapKit Example

--- WeDontNeedStoryboard/ViewController.swift
+++ WeDontNeedStoryboard/ViewController.swift
@@ -6,14 +6,91 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+
+    private func checkLocationAuthorization() {
+        guard let locationManager, let location = locationManager.location else { return }
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways,
+                .authorizedWhenInUse:
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 750, longitudinalMeters: 750)
+            mapView.setRegion(region, animated: true)
+            print("")
+        case .notDetermined,
+                .restricted:
+            print("Location Service Can Not Be Determinded or Resitred")
+        case .denied:
+            print("Location Services Has Been Denied!")
+        default:
+            print("Unknow Error! Unable To Get Location!")
+        }
+    }
+}
+
+final class ViewController: UIViewController {
+    var locationManager: CLLocationManager?
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 12
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.placeholder = "Search Location!"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        requestPermissionLocation()
     }
 
+    private func requestPermissionLocation() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+        locationManager?.requestAlwaysAuthorization()
 
+    }
+
+    private func setupUI() {
+        view.addSubview(mapView)
+        view.addSubview(searchTextField)
+        view.bringSubviewToFront(searchTextField)
+        /// Add Constraints MapView
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        /// Add Constraints Search TextField
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width / 1.2).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
+    }
+    
 }
 
